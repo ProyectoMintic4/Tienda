82d45ea0962eaaee593e3786b4e1718c91802a93
SSRamiSanta <ssrs_10@hotmail.com>
2021-11-28 03:15:43 -0500

    Sprint 3

--- app/src/main/java/com/example/tienda/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/tienda/view/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tienda
+package com.example.tienda.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tienda.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
